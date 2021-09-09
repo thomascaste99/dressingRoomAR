c79f85c5daaaf64ae763677fcd5370b72653244d
Thomas Castellani <castellani.thomas.iti@gmail.com>
2021-09-09 18:01:27 +0200

    trying to resolve background and resolution issues

--- js/three.js
+++ js/three.js
@@ -1,10 +1,14 @@
 window.onload = function() {
     
+    const video = document.getElementById("video");
+
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer( { alpha: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
+    scene.background = new THREE.Color( 0xff0000 );
     document.body.appendChild( renderer.domElement );
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
